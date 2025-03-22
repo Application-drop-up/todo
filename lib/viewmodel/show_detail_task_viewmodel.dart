e283b51675cfbc0e27bf3bde3cefa2d951b7da54
Application-drop-up <msts.oo0131@gmail.com>
2025-03-22 14:57:41 +0900

    feat: add-show_detail_task_viewmodel

---
+++ lib/viewmodel/show_detail_task_viewmodel.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo/model/show_detail_task.dart';
+
+class ShowDetailTaskViewModel extends ChangeNotifier {
+
+}
---
+++ test/unit/viewmodel/show_detail_task_viewmodel_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo/viewmodel/show_detail_task_viewmodel.dart';
+import 'package:todo/model/show_detail_task.dart';
+
+void main() {
+  group('ShowDetailTaskViewModel Test', () {
+    late ShowDetailTaskViewModel viewModel;
+
+    setUp(() {
+      viewModel = ShowDetailTaskViewModel();
+    });
+
+    test('should return a task when a valid ID is provided', () {
+      final task = viewModel.getTaskById("1");
+
+      expect(task, isNotNull);
+      expect(task!.id, "1");
+      expect(task.title, "Task 1");
+      expect(task.content, "Description 1");
+      expect(task.dueDate, DateTime(2025, 5, 10));
+      expect(task.isCompleted, false);
+    });
+
+    test('should return null when an invalid ID is provided', () {
+      final task = viewModel.getTaskById("999");
+
+      expect(task, isNotNull);
+      expect(task!.id, "");
+      expect(task.title, "Not Found");
+      expect(task.content, "No description available");
+      expect(task.dueDate, null);
+      expect(task.isCompleted, false);
+    });
+
+    test('should return all tasks', () {
+      final tasks = viewModel.tasks;
+
+      expect(tasks.length, 2);
+      expect(tasks[0].id, "1");
+      expect(tasks[1].id, "2");
+    });
+  });
+}
