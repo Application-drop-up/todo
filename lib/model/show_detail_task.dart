1c33d9ce24a87770a468eca5f881ae4aa52cf6ef
Application-drop-up <msts.oo0131@gmail.com>
2025-03-22 14:58:11 +0900

    feat: add_test-show_detail_task_viewmodel

--- lib/model/show_detail_task.dart
+++ lib/model/show_detail_task.dart
@@ -1,13 +1,15 @@
 class Task {
   final String id;
   final String title;
-  final String description;
+  final String content;
   final DateTime? dueDate;
+  final bool isCompleted;
 
   Task({
     required this.id,
     required this.title,
-    required this.description,
+    required this.content,
     this.dueDate,
+    required this.isCompleted,
   });
 }
